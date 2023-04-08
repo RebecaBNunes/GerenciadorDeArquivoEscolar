25a5479db9ed70c4f91123b27feafbda2e489197
Angelus Trindade <angelusrt@gmail.com>
2023-04-08 16:47:25 -0300

    Angelus_Consulta

--- main.c
+++ main.c
@@ -30,8 +30,7 @@ void cadastrar();
 int validarCpf(char *cpf);
 void consultar();
 
-int main()
-{
+int main(){
     setlocale(LC_ALL, "Portuguese");
 
     menu();
@@ -41,6 +40,7 @@ int main()
 
 void menu(){
     int escolha;
+
     while(escolha != 5){
         printf("-----------------------------\n"
            "%15s\n", "MENU");
@@ -55,24 +55,12 @@ void menu(){
         scanf("%d", &escolha);
 
         switch(escolha){
-        case 1:
-            cadastrar();
-            break;
-        case 2:
-            consultar();
-            break;
-        case 3:
-            // remover();
-            break;
-        case 4:
-            //alterar();
-            break;
-        case 5:
-            printf("Encerrando programa...");
-            break;
-        default:
-            printf("Op��o inv�lida, escolha novamente!\n");
-            break;
+            case 1: cadastrar(); break;
+            case 2: consultar(); break;
+            // case 3: remover(); break;
+            // case 4: alterar(); break;
+            case 5: printf("Encerrando programa..."); break;
+            default: printf("Opção inválida, escolha novamente!\n"); break;
         }
     }
 }
@@ -80,16 +68,14 @@ void menu(){
 void cadastrar(){
     FILE *registro;
     Aluno alunos[TAM_LISTA];
+
     int index = 0;
     char opcao = 'n';
+    
     if((registro = fopen("registro-alunos.txt", "a+")) == NULL){
         printf("! Ocorreu algum problema ao tentar abrir o arquivo registro-aluno.txt\n");
     }
     else{
-        /*
-            /// precisa cadastrar notas.
-            /// fazer valida��o do cpf
-        */
         printf("\n-----------------------------------------------------------------------\n");
         printf("                            CADASTRO DE ALUNO:                           \n");
         printf("-----------------------------------------------------------------------\n");
@@ -97,60 +83,62 @@ void cadastrar(){
         printf("-----------------------------------------------------------------------\n");
 
         do{
+            Aluno aluno = alunos[index];
+            
             printf("Nome: ");
-            scanf(" %49[^\n]s", alunos[index].nome);
+            scanf("%49[^\n]s", aluno.nome);
             printf("cpf: FORMATO(xxxxxxxxxxx) ");
-            scanf(" %11[^\n]s", alunos[index].cpf);
+            scanf("%11[^\n]s", aluno.cpf);
 
             // validacao do CPF:
-            int ehValido = validarCpf(alunos[index].cpf);
+            int ehValido = validarCpf(aluno.cpf);
             while(ehValido == 0){
                 printf("CPF inv�lido! Digite novamente: ");
-                scanf(" %11[^\n]s", alunos[index].cpf);
-                ehValido = validarCpf(alunos[index].cpf);
+                scanf(" %11[^\n]s", aluno.cpf);
+                ehValido = validarCpf(aluno.cpf);
             }
 
             printf("Data de Nascimento: FORMATO(dd MM yyyy)\n");
             printf("Dia: ");
-            scanf("%hd", &alunos[index].dataNascimento.dia);
+            scanf("%hd", &aluno.dataNascimento.dia);
             printf("Mês: ");
-            scanf("%hd", &alunos[index].dataNascimento.mes);
+            scanf("%hd", &aluno.dataNascimento.mes);
             printf("Ano: ");
-            scanf("%hd", &alunos[index].dataNascimento.ano);
+            scanf("%hd", &aluno.dataNascimento.ano);
 
             printf("Matricula: ");
-            scanf("%ld", &alunos[index].matricula);
+            scanf("%ld", &aluno.matricula);
 
             printf("Notas: \n");
             printf("Primeira nota: ");
-            scanf("%f", &alunos[index].notas[0]);
+            scanf("%f", &aluno.notas[0]);
             printf("Segunda nota: ");
-            scanf("%f", &alunos[index].notas[1]);
+            scanf("%f", &aluno.notas[1]);
             printf("Terceira nota: ");
-            scanf("%f", &alunos[index].notas[2]);
+            scanf("%f", &aluno.notas[2]);
             printf("Quarta nota: ");
-            scanf("%f", &alunos[index].notas[3]);
+            scanf("%f", &aluno.notas[3]);
 
             fprintf(
                 registro, 
                 "%s %s %ld %hd %hd %hd %.2f %.2f %.2f %.2f\n", 
-                alunos[index].nome, 
-                alunos[index].cpf, 
-                alunos[index].matricula,
-                alunos[index].dataNascimento.dia,
-                alunos[index].dataNascimento.mes,
-                alunos[index].dataNascimento.ano,
-                alunos[index].notas[0],
-                alunos[index].notas[1],
-                alunos[index].notas[2],
-                alunos[index].notas[3]
+                aluno.nome, 
+                aluno.cpf, 
+                aluno.matricula,
+                aluno.dataNascimento.dia,
+                aluno.dataNascimento.mes,
+                aluno.dataNascimento.ano,
+                aluno.notas[0],
+                aluno.notas[1],
+                aluno.notas[2],
+                aluno.notas[3]
             );
 
             index++;
 
             printf("continuar cadastro de alunos? FORMATO(s / n)");
             scanf(" %c", &opcao);
-        } while((opcao != 'n'));
+        } while(opcao != 'n');
 
         fclose(registro);
     }
@@ -160,39 +148,43 @@ void cadastrar(){
 int validarCpf(char *cpf){
     int digito1 = 0;
     int digito2 = 0;
+    
     for(int i = 0, j = 10; i < strlen(cpf)-2; i++, j--){
         digito1 += (cpf[i]-48) * j;
     }
+    
     digito1 %= 11;
-    if(digito1 < 2){
+    
+    if(digito1 < 2)
         digito1 = 0;
-    }
-    else {
+    else 
        digito1 = 11 - digito1;
-    }
 
-    if((cpf[9]-48) != digito1){
-        return 0;
-    }
+    if((cpf[9]-48) != digito1) return 0;
+
     for(int i = 0, j =11; i < strlen(cpf)-1; i++, j--){
         digito2 += (cpf[i]-48) * j;
     }
+
     digito2 %= 11;
-    if(digito2 < 2){
+
+    if(digito2 < 2)
         digito2 = 0;
-    }
-    else{
+    else
         digito2 = 11 - digito2;
-    }
-    if((cpf[10]-48) != digito2){
-        return 0;
-    }
+    
+    if((cpf[10]-48) != digito2) return 0;
+
     return 1;
 }
 
 void consultar(){
     FILE *arquivoRead;
     Aluno alunos[TAM_LISTA];
+    char ch;
+    char opcao = 'n';
+    
+
     //int index = 0;
     if((arquivoRead = fopen("registro-alunos.txt", "r")) == NULL){
         printf("! Ocorreu algum problema ao tentar abrir o arquivo registro-aluno.txt\n");
@@ -202,11 +194,16 @@ void consultar(){
         printf("                            LISTAGEM DE ALUNOS:                           \n");
         printf("-----------------------------------------------------------------------\n");
 
-        while(!feof(arquivoRead)){
-            //c�digo referente � leitura do arquivo com os registros dos alunos
+        do{
+            while((ch = getc(arquivoRead)) != EOF){
+                printf("%c", ch);
+            }
+            printf("\n");
 
-        }
-
+            printf("continuar consulta de alunos? FORMATO(s / n)");
+            scanf(" %c", &opcao);
+        } while(opcao != 'n');
+    
         fclose(arquivoRead);
     }
 }
