9d31a0ba87cba32656b24b0b4c01b7f81962b66d
Angelus Trindade <angelusrt@gmail.com>
2023-04-24 16:45:50 -0300

    Alterar adicionado

--- main.c
+++ main.c
@@ -1,34 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define TAM_NOME 50
-#define TAM_CPF 12
-#define TAM_LISTA 20
-#define BUFFER_SIZE 1000
-
-struct Data{
-    unsigned short dia;
-    unsigned short mes;
-    unsigned short ano;
-};
-
-typedef struct Data Data;
-
-struct Aluno{
-    char nome[TAM_NOME];
-    char cpf[TAM_CPF];
-    Data dataNascimento;
-    long matricula;
-    float notas[4];
-};
-
-typedef struct Aluno Aluno;
+#include <stdbool.h>
+#include <time.h>
+#include "utils.h"
 
 void menu();
 void cadastrar();
-int validarCpf(char *cpf);
 void consultar();
+void alterar();
 void remover();
 
 int main(){
@@ -42,7 +22,7 @@ void menu(){
 
     while(escolha != 5){
         printf("-----------------------------\n");
-        printf("             Menu            ");
+        printf("             Menu            \n");
         printf("-----------------------------\n");
         printf("%d: %10s\n", 1, "Cadastrar Aluno\n");
         printf("%d: %10s\n", 2, "Consultar Aluno\n");
@@ -50,14 +30,14 @@ void menu(){
         printf("%d: %10s\n", 4, "Alterar aluno\n");
         printf("%d: %10s\n", 5, "Sair do programa\n");
         printf("-----------------------------\n");
-        printf("Escolha uma opção: (1 a 5): ");
+        printf("Escolha uma opção: (1 a 5): \n");
         scanf("%d", &escolha);
 
         switch(escolha){
             case 1: cadastrar(); break;
             case 2: consultar(); break;
             case 3: remover(); break;
-            // case 4: alterar(); break;
+            case 4: alterar(); break;
             case 5: printf("Encerrando programa..."); break;
             default: printf("Opção inválida, escolha novamente!\n"); break;
         }
@@ -65,191 +45,154 @@ void menu(){
 }
 
 void cadastrar(){
-    FILE *arquivo = NULL;
-    Aluno alunos[TAM_LISTA];
-
-    int index = 0;
-    char opcao = 'n';
-
-    arquivo = fopen("arquivo-alunos.txt", "a+");
-    
-    if(arquivo == NULL){
-        printf("O arquivo não foi aberto\n");
-        exit(EXIT_FAILURE);
-    }
+    FILE *arquivo = tentarAbrirArquivo("a+");
 
     printf("-----------------------------\n");
-    printf("      CADASTRO DE ALUNO:     ");
+    printf("      CADASTRO DE ALUNO:     \n");
     printf("-----------------------------\n");
     printf("Preencha os campos solicitados\n");
     printf("-----------------------------\n");
+    
+    Aluno aluno;
+    criarAluno(&aluno);
 
-    do{
-        Aluno aluno = alunos[index];
-        
-        printf("Nome: ");
-        scanf(" %49[^\n]s", aluno.nome);
-        printf("cpf: FORMATO(xxxxxxxxxxx) ");
-        scanf(" %11[^\n]s", aluno.cpf);
+    char *nomeResto = pegarResto(aluno.nome, NOME_TAMANHO);
+    char *matriculaResto = pegarResto(aluno.matricula, MATRICULA_TAMANHO);
+    
+    fprintf(
+        arquivo, 
+        "%s%s %s %s%s %.2d %.2d %.4d %.2f %.2f\n", 
+        aluno.nome,
+        nomeResto,
+        aluno.cpf, 
+        aluno.matricula,
+        matriculaResto,
+        aluno.dataNascimento.tm_mday,
+        aluno.dataNascimento.tm_mon,
+        aluno.dataNascimento.tm_year,
+        aluno.notas[0],
+        aluno.notas[1]
+    );
 
-        // validacao do CPF:
-        int ehValido = validarCpf(aluno.cpf);
-        while(ehValido == 0){
-            printf("CPF inválido! Digite novamente: ");
-            scanf(" %11[^\n]s", aluno.cpf);
-            ehValido = validarCpf(aluno.cpf);
-        }
-
-        printf("Data de Nascimento: FORMATO(dd MM yyyy)\n");
-        printf("Dia: ");
-        scanf("%hd", &aluno.dataNascimento.dia);
-        printf("Mês: ");
-        scanf("%hd", &aluno.dataNascimento.mes);
-        printf("Ano: ");
-        scanf("%hd", &aluno.dataNascimento.ano);
-
-        printf("Matricula: ");
-        scanf("%ld", &aluno.matricula);
-
-        printf("Notas: \n");
-        printf("Primeira nota: ");
-        scanf("%f", &aluno.notas[0]);
-        printf("Segunda nota: ");
-        scanf("%f", &aluno.notas[1]);
-        printf("Terceira nota: ");
-        scanf("%f", &aluno.notas[2]);
-        printf("Quarta nota: ");
-        scanf("%f", &aluno.notas[3]);
-
-        fprintf(
-            arquivo, 
-            "%s %s %ld %hd %hd %hd %.2f %.2f %.2f %.2f\n", 
-            aluno.nome, 
-            aluno.cpf, 
-            aluno.matricula,
-            aluno.dataNascimento.dia,
-            aluno.dataNascimento.mes,
-            aluno.dataNascimento.ano,
-            aluno.notas[0],
-            aluno.notas[1],
-            aluno.notas[2],
-            aluno.notas[3]
-        );
-
-        index++;
-
-        printf("continuar cadastro de alunos? FORMATO(s / n)");
-        scanf(" %c", &opcao);
-    } while(opcao != 'n');
-
+    free(nomeResto);
+    free(matriculaResto);
     fclose(arquivo);
-}
-
-int validarCpf(char *cpf){
-    short digito1 = 0, digito2 = 0, ascii = 0;
-
-    for(int i = 0, j = 10; i < strlen(cpf) - 2; i++, j--){
-        ascii = (short)(cpf[i]);
-        digito1 += ( ascii - 48 ) * j;
-    }
-    
-    digito1 %= 11;
-    
-    digito1 = (digito1 < 2) ? 0 : 11 - digito1;
-
-    if((cpf[9] - 48) != digito1) return 0;
-
-    for(int i = 0, j = 11; i < strlen(cpf)-1; i++, j--){
-        ascii = (short)(cpf[i]);
-        digito2 += (ascii - 48) * j;
-    }
-
-    digito2 %= 11;
-
-    digito2 = (digito2 < 2) ? 0 : 11 - digito2;
-    
-    ascii = (short)(cpf[10]);
-
-    if((ascii - 48) != digito2) return 0;
-    
-    return 1;
+    esperarInput();
 }
 
 void consultar(){
-    FILE *arquivo = NULL;
-    Aluno alunos[TAM_LISTA];
-    char ch;
-
-    arquivo = fopen("arquivo-alunos.txt", "r");
-
-    if(arquivo == NULL){
-        printf("O arquivo não foi aberto\n");
-        exit(EXIT_FAILURE);
-    }
+    FILE *arquivo = tentarAbrirArquivo("r");
 
     printf("\n-----------------------------\n");
-    printf("      LISTAGEM DE ALUNOS:      \n");
-    printf("-----------------------------");
+    printf("      CONSULTA DE ALUNOS:    \n");
+    printf("-----------------------------\n");
+
+    char ch;
+    long arquivoTamanho = pegarArquivoTamanho(arquivo);
+    long linhas = arquivoTamanho / LINHA_TAMANHO;
+    long datas[linhas];
 
     while((ch = getc(arquivo)) != EOF){
-        printf("%c", ch);
+        long posicaoAtual = ftell(arquivo);
+        long linhaAtual = posicaoAtual / LINHA_TAMANHO;
+
+        bool ehInicioDaData = posicaoAtual == linhaAtual * LINHA_TAMANHO + DATA_COMECO;
+        
+        if(ehInicioDaData){
+            char data[DATA_TAMANHO];            
+            for (size_t i = 0; i < DATA_TAMANHO; i++)
+                data[i] = getc(arquivo);
+            
+            int dia, mes, ano; 
+            sscanf(data, "%d %d %d", &dia, &mes, &ano);
+
+            bool ehValido = data[0] != ' ';
+            if(!ehValido) {
+                printf(" ");
+                fseek(arquivo, - DATA_TAMANHO + 1, SEEK_CUR);
+                continue;
+            }
+
+            struct tm dataFormatada = {
+                .tm_year = ano - 1900,
+                .tm_mon = mes - 1,
+                .tm_mday = dia 
+            };        
+
+            time_t hoje = time(NULL);
+            struct tm *hojeFormatado = localtime(&hoje);
+            
+            printf("%d", pegarIdade(*hojeFormatado, dataFormatada));
+            
+            fseek(arquivo, -DATA_TAMANHO + 1, SEEK_CUR);
+        } else if (
+            posicaoAtual > linhaAtual * LINHA_TAMANHO + DATA_COMECO &&
+            posicaoAtual <= linhaAtual * LINHA_TAMANHO + (DATA_COMECO + DATA_TAMANHO)
+        ){
+            printf(" ");
+        } else {
+            printf("%c", ch);
+        }
     }
 
     printf("\n");
 
     fclose(arquivo);
+    esperarInput();
+}
+
+void alterar(){
+    FILE *arquivo = tentarAbrirArquivo("r+");
+
+    printf("\n-----------------------------\n");
+    printf("     ALTERAÇÃO DE ALUNO:     \n");
+    printf("-----------------------------\n");
+
+    long arquivoTamanho = pegarArquivoTamanho(arquivo);
+    long linhas = arquivoTamanho / LINHA_TAMANHO;
+
+    long indice = -1;
+    tentarCriarIndice(arquivo, &indice, linhas);
+
+    Aluno aluno;
+    criarAluno(&aluno);
+
+    char alunoFormatado[LINHA_TAMANHO];
+    formatarAluno(aluno, alunoFormatado);
+
+    fseek(arquivo, indice, SEEK_SET);
+
+    for (size_t i = 0; i < LINHA_TAMANHO - 1; i++)        
+        fputc(alunoFormatado[i], arquivo);
+
+    printf("\n");
+    printf("Aluno alterado com sucesso!\n");
+
+    fclose(arquivo);
+    esperarInput();
 }
 
 void remover(){
-    FILE *arquivo = NULL;
-    // Aluno alunos[TAM_LISTA];
-    char ch = 'a';
+    FILE *arquivo = tentarAbrirArquivo("r+");
+
+    printf("\n-----------------------------\n");
+    printf("     REMOVEÇÃO DE ALUNOS:    \n");
+    printf("-----------------------------\n");
+
+    long arquivoTamanho = pegarArquivoTamanho(arquivo);
+    long linhas = arquivoTamanho / LINHA_TAMANHO;
+
     long indice = -1;
-    long quantidadeDeLinhas = 0;
-    long quantidadeParaApagar = 0;
-    long indiceReal = 0;
+    tentarCriarIndice(arquivo, &indice, linhas);
 
-    printf("Digite a linha que você deseja excluir: (começando de 0) ");
-    scanf("%ld", &indice);
+    fseek(arquivo, indice, SEEK_SET);
 
-    arquivo = fopen("arquivo-alunos.txt", "r+");
-
-    if(arquivo == NULL){
-        printf("Arquivo inacessível ou não existe.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    do {
-        ch = fgetc(arquivo);
-        if(ch == '\n') quantidadeDeLinhas++;
-        if(quantidadeDeLinhas < indice) indiceReal++;
-    } while(ch != EOF);
-    
-    indiceReal++;
-
-    if(quantidadeDeLinhas < indice && indice < 0){
-        printf("Índice maior que quantidade de linhas ou não válido.\n");
-        fclose(arquivo);
-        exit(EXIT_FAILURE);
-    }
-
-    fseek(arquivo, indiceReal, SEEK_SET);
-    
-    do {
-        ch = fgetc(arquivo);
-        quantidadeParaApagar++;
-    } while(ch != '\n');
-
-    quantidadeParaApagar--;
-
-    fseek(arquivo, indiceReal, SEEK_SET);
-
-    indice = 0;
-    
-    do{
+    for (size_t i = 0; i < LINHA_TAMANHO - 1; i++)
         fputc(' ', arquivo);
-        indice++;
-    } while (indice < quantidadeParaApagar);
+    
+    printf("\n");
+    printf("Aluno excluído com sucesso!\n");
 
     fclose(arquivo);
+    esperarInput();
 }
